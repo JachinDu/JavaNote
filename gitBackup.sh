09df09e9d574630b9793ea181d208b09bd25b4c7
Jachin <jiachengdd@gmail.com>
2020-06-04 21:01:42 +0800

    commitMsg

--- gitBackup.sh
+++ gitBackup.sh
@@ -1,12 +1,7 @@
+git add .
+read commitMsg
+git commit -m commitMsg
+git push gitee master
+git push javanote master
+echo "Success!"
 
-if [ -n "$1" ]; then
-	git add .
-	git commit -m "$1"
-	git push gitee master
-	git push javanote master
-	git status
-	echo "backup to gitee & github sucessfully!"
-else
-	echo "please add commit message!"
-fi
-exit 0

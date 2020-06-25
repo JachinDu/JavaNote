daaf8138147d4bde33ab6c0f7623a8185c86be74
Jachin <jiachengdd@gmail.com>
2020-06-25 22:52:27 +0800

    update sth

--- gitBackup.sh
+++ gitBackup.sh
@@ -1,10 +1,11 @@
-msg=$1
-if [ -n "$msg" ]; then
+#msg=$1
+#if [ -n "$msg" ]; then
 	git add .
-	git commit -m "${msg}"
-	git push javanote master
+	#git commit -m "${msg}"
+	git commit --amend
+    git push javanote master
 	git push gitee master
 	echo "Success!"
-else
-	echo "please add commit msg!"
-fi
+#else
+#	echo "please add commit msg!"
+#fi
